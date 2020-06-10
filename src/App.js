546eeaa3513cb586c8dac3e668fb2965fe7ce558
altaf <atmalik24@gmail.com>
2020-06-10 11:04:51 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [isOn, setOn] = useState(false);
+  let [temp, setTemp] = useState(22);
+  const on = isOn ? 'On': 'Off';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${on}`}>
+      <button onClick={() => setOn(!isOn)}>{on}</button>
+      <h2>Temperature in Celsius {temp}</h2>
+      <button onClick={() => setTemp(++temp)}>+</button>
+      <button onClick={() => setTemp(--temp)}>-</button>
     </div>
   );
 }
